b465b8b88f49ebc7945f9b9fbe2c7a9d58662fa7
Devluizpaulo <136992113+Devluizpaulo@users.noreply.github.com>
2026-05-30 19:37:15 -0300

    c

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,12 @@
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
 import { FlatCompat } from '@eslint/eslintrc'
 
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
 const compat = new FlatCompat({
-  // import.meta.dirname is available after Node.js v20.11.0
-  baseDirectory: import.meta.dirname,
+  baseDirectory: __dirname,
 })
 
 const eslintConfig = [
@@ -14,13 +18,13 @@ const eslintConfig = [
       'build/**',
     ],
   },
-  ...compat.config({
-    extends: ['next'],
+  ...compat.extends('next/core-web-vitals'),
+  {
     rules: {
       'react/no-unescaped-entities': 'off',
       '@next/next/no-page-custom-font': 'off',
     },
-  }),
+  },
 ]
 
 export default eslintConfig
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true, // Habilita o modo estrito do React
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
