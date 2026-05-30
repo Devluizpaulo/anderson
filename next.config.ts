ba9a4b5ace96c13ced808de1afd9da950f689be7
Devluizpaulo <136992113+Devluizpaulo@users.noreply.github.com>
2026-05-30 19:38:42 -0300

    sd

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true, // Habilita o modo estrito do React
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
 };
 
 export default nextConfig;
